8d0ba4623e19a9b19c817e1c0eb52430a6c93136
Admin - Luis Alexander Contreras Alfaro <admluis@grupocalvo.com.sv>
2020-03-12 12:19:01 -0600

    primera version

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+require_once("index.html");
+require_once("blog-single.html");
+require_once("blog.html");
+require_once("contact.html");
+require_once("about.html");
+require_once("main.html");
+require_once("project.html");
+require_once("team.html");
+
+
+
+?>
